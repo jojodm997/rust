4706063ae014c23fbcae51982020d6dc9eff2528
Fauzan <fauzandwimulyono@gmail.com>
2024-07-23 19:47:40 +0700

    Add something

---
+++ guess_game/target/debug/deps/guess_game-082039ad7e69470a.d
@@ -0,0 +1,5 @@
+/home/fauzan/code/rust/guess_game/target/debug/deps/guess_game-082039ad7e69470a: src/main.rs
+
+/home/fauzan/code/rust/guess_game/target/debug/deps/guess_game-082039ad7e69470a.d: src/main.rs
+
+src/main.rs:
---
+++ guess_game/target/debug/guess_game.d
@@ -0,0 +1,1 @@
+/home/fauzan/code/rust/guess_game/target/debug/guess_game: /home/fauzan/code/rust/guess_game/src/main.rs
